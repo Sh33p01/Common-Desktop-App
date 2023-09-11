468d4d2ebf5ff1be6cbecb18843bb3ec4750d5b9
Sh33p <Kenan.m001@gmail.com>
2023-09-11 21:03:49 +0200

    Finished Username textbox, Window Controls and Logo

--- Library Application/View/Login.xaml.cs
+++ Library Application/View/Login.xaml.cs
@@ -26,7 +26,18 @@ namespace Library_Application.View
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
 
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
