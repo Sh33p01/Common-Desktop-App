418b3a5aa09d416f9702ff6b7a1d4f4396cf8eac
Sh33p <Kenan.m001@gmail.com>
2023-10-09 06:45:00 +0200

    Syncing with PC

---
+++ Library Application/ViewModels/LoginViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Library_Application.ViewModels
+{
+    internal class LoginViewModel : ViewModelBase
+    {
+        //Fields
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisable = true;
+
+        //Properties
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                Username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public SecureString password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(password));
+            }
+        }
+        public string ErrorMessage
+        {
+            get
+            {
+                return _errorMessage;
+            }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisable
+        {
+            get
+            {
+                return IsViewVisable;
+            }
+            set
+            {
+                _isViewVisable = value;
+                OnPropertyChanged(nameof(IsViewVisable));
+            }
+        }
+
+        //-> Commands
+        public ICommand LoginCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+        
+
+        //Constructors
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelCommand(ExecuteRecoverPassCommand);
+        }
+
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteRecoverPassCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Library Application/ViewModels/ViewModelCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Library_Application.ViewModels
+{
+    internal class ViewModelCommand : ICommand
+    {
+
+        private readonly Action<object> _executeAction; //encapsulate a method, i.e pass a method as a parameter
+        private readonly Predicate<object> _canExecuteAction; //determine if the action can be executed or not.
+
+        //Constructors
+        public ViewModelCommand(Action<object> executeAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = null;
+        }
+        public ViewModelCommand(Action<object> executeAction, Predicate<object> canExecuteAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = canExecuteAction;
+        }
+
+
+        //Events
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        //Methods
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteAction == null ? true : _canExecuteAction(parameter);
+        }
+         
+        public void Execute(object parameter)
+        {
+            _executeAction(parameter);
+        }
+    }
+}
